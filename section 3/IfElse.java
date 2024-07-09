3624805c73a005058fbfae2db1689ac58f6d626d
yunlim <yunlimol@outlook.com>
2024-07-09 22:52:44 +0800

    Add files via upload

---
+++ section 3/IfElse.java
@@ -0,0 +1,22 @@
+import javax.swing.plaf.synth.SynthSeparatorUI;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int grade = 40;
+
+        if(grade >=80 ){
+            System.out.println("You got an A");
+        } else if(grade >= 70){
+            System.out.println("You got a B");
+        } else if(grade >=60){
+            System.out.println("You got a C");
+        } else{
+            System.out.println("Study hard next time!");
+        }
+
+
+
+
+
+    }
+}
---
+++ section 4/Parameters.java
@@ -0,0 +1,18 @@
+public class Parameters {
+    public static void main(String[] args) {
+    double measure1 =  measureRectangle(2.2, 4.3);
+    double measure2 = measureRectangle(3.2, 4.1);
+System.out.println("area of rectangle is " + measure1);
+
+    }
+
+public static double measureRectangle(double length, double width){
+    double area = length * width;
+    return area;
+}
+
+public static void stringPrinter(double length, double width, double area){
+
+}
+
+}
