4ac69e76942325fd499247155eeb4b70a47778b8
yunlim <yunlimol@outlook.com>
2024-07-09 23:14:01 +0800

    dont need it

--- 1. Getting Started/Challenge 1/YourInitials.java
+++
@@ -1,5 +0,0 @@
-public class YourInitials {
-    public static void main(String[] args) {
-         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
-    }
-}
